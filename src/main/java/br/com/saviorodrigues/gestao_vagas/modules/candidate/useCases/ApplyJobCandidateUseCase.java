775c8bea1c9183830087b265d522b0654177ca9d
rodriguessavio <capar.2022113tads0049@aluno.ifpi.edu.br>
2026-02-24 11:23:04 -0300

    Adicionando código inicial ApplyJobCandidateUseCase e testes iniciais de ApplyJobCandidateUseCase

---
+++ src/main/java/br/com/saviorodrigues/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
@@ -0,0 +1,33 @@
+package br.com.saviorodrigues.gestao_vagas.modules.candidate.useCases;
+
+import br.com.saviorodrigues.gestao_vagas.exceptions.JobNotFoundException;
+import br.com.saviorodrigues.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.saviorodrigues.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.saviorodrigues.gestao_vagas.modules.company.repositories.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class ApplyJobCandidateUseCase {
+
+    @Autowired
+    public JobRepository jobRepository;
+
+    @Autowired
+    public CandidateRepository candidateRepository;
+
+    public void execute(UUID idCandidate, UUID idJob){
+        this.candidateRepository.findById(idCandidate)
+        .orElseThrow( () -> {
+            throw new UserNotFoundException();
+        });
+
+        this.jobRepository.findById(idJob)
+        .orElseThrow( () -> {
+            throw new JobNotFoundException();
+        });
+
+    }
+}
---
+++ src/test/java/br/com/saviorodrigues/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
@@ -0,0 +1,63 @@
+package br.com.saviorodrigues.gestao_vagas.modules.candidate.useCases;
+
+import br.com.saviorodrigues.gestao_vagas.exceptions.JobNotFoundException;
+import br.com.saviorodrigues.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.saviorodrigues.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.saviorodrigues.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.saviorodrigues.gestao_vagas.modules.company.repositories.JobRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ApplyJobCandidateUseCaseTest {
+
+    @InjectMocks
+    private ApplyJobCandidateUseCase applyJobCandidateUseCase;
+
+    @Mock
+    private CandidateRepository candidateRepository;
+
+    @Mock
+    private JobRepository jobRepository;
+
+    @Test
+    @DisplayName("Shoud not be able to apply job with candidate not found")
+    public void should_not_be_able_to_apply_job_with_candidate_not_found() {
+        try{
+            applyJobCandidateUseCase.execute(null, null);
+        }
+        catch(Exception ex){
+            assertThat(ex).isInstanceOf(UserNotFoundException.class);
+        }
+
+    }
+
+    @Test
+    @DisplayName("Shoud not be able to apply job with job not found")
+    public void should_not_be_able_to_apply_job_with_job_not_found() {
+
+        var idCandidate = UUID.randomUUID();
+
+        var candidate = new CandidateEntity();
+        candidate.setId(idCandidate);
+
+        when(candidateRepository.findById(idCandidate)).thenReturn(Optional.of(candidate));
+
+        try {
+            applyJobCandidateUseCase.execute(idCandidate, null);
+        } catch (Exception e){
+            assertThat(e).isInstanceOf(JobNotFoundException.class);
+        }
+    }
+}
