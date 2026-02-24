2e307840b03920674946c0be6919499a5ffe81e9
rodriguessavio <capar.2022113tads0049@aluno.ifpi.edu.br>
2026-02-24 11:46:37 -0300

    Criando entidade ApplyJobEntity e ApplyJobRepository

--- src/main/java/br/com/saviorodrigues/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
+++ src/main/java/br/com/saviorodrigues/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
@@ -3,6 +3,7 @@ package br.com.saviorodrigues.gestao_vagas.modules.candidate.useCases;
 import br.com.saviorodrigues.gestao_vagas.exceptions.JobNotFoundException;
 import br.com.saviorodrigues.gestao_vagas.exceptions.UserNotFoundException;
 import br.com.saviorodrigues.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.saviorodrigues.gestao_vagas.modules.candidate.repository.ApplyJobRepository;
 import br.com.saviorodrigues.gestao_vagas.modules.company.repositories.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,9 @@ public class ApplyJobCandidateUseCase {
 
     @Autowired
     public CandidateRepository candidateRepository;
+
+    @Autowired
+    public ApplyJobRepository applyJobRepository;
 
     public void execute(UUID idCandidate, UUID idJob){
         this.candidateRepository.findById(idCandidate)
