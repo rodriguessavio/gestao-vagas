734627de3cf42b1be8413452d2d02db7d492b22b
rodriguessavio <capar.2022113tads0049@aluno.ifpi.edu.br>
2026-08-10 19:59:10 -0300

    feat(user): Criação de dto de autenticação  de usuário

--- src/main/java/br/com/saviorodrigues/gestao_vagas/security/SecurityCompanyFilter.java
+++ src/main/java/br/com/saviorodrigues/gestao_vagas/security/SecurityCompanyFilter.java
@@ -9,15 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Collections;
 
 @Component
-public class SecurityFilter extends OncePerRequestFilter {
+public class SecurityCompanyFilter extends OncePerRequestFilter {
 
     @Autowired
     private JWTProvider jwtProvider;
--- src/main/java/br/com/saviorodrigues/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/saviorodrigues/gestao_vagas/security/SecurityConfig.java
@@ -15,15 +15,17 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 public class SecurityConfig {
 
     @Autowired
-    private SecurityFilter securityFilter;
+    private SecurityCompanyFilter securityCompanyFilterFilter;
 
     @Autowired
     private SecurityCandidateFilter securityCandidateFilter;
 
-    private static final String[] SWAGGER_LIST = {
+    private static final String[] PERMIT_ALL_LIST = {
             "/swagger-ui/**",
             "/v3/api-docs/**",
             "/swagger-resources/**",
+            "/actuator/**",
+            "/swagger-ui.html"
     };
 
     @Bean
@@ -34,11 +36,11 @@ public class SecurityConfig {
                         .requestMatchers("/company/").permitAll()
                         .requestMatchers("/company/auth").permitAll()
                         .requestMatchers("/candidate/auth").permitAll()
-                        .requestMatchers(SWAGGER_LIST).permitAll();
+                        .requestMatchers(PERMIT_ALL_LIST).permitAll();
                     auth.anyRequest().authenticated();
                 })
                 .addFilterBefore(securityCandidateFilter, BasicAuthenticationFilter.class)
-                .addFilterBefore(securityFilter, BasicAuthenticationFilter.class)
+                .addFilterBefore(securityCompanyFilterFilter, BasicAuthenticationFilter.class)
         ;
         return http.build();
     }
